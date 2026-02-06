6f327a7dd938ee168a3d466b8f6eb07bd04a7b17
Denis Narminov~ <narminovd@gmail.com>
2026-02-06 21:44:50 +0300

    Remove build artifacts and add .gitignore

--- build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/QtProjects/Travel
-# Build directory: C:/QtProjects/Travel/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("TravelTests")
