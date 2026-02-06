0b777e323943b2f72f0a4b0f5bb8b4c45ea66e8f
Denis Narminov~ <narminovd@gmail.com>
2026-02-06 20:45:56 +0300

    Remove build artifacts (binary files)

--- build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Travel_FILE C:/QtProjects/Travel/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/Travel.exe)
-set(__QT_DEPLOY_TARGET_Travel_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Travel_RUNTIME_DLLS C:/Qt/6.10.1/mingw_64/bin/Qt6Network.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6PrintSupport.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Core.dll)
