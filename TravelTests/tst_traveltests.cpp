c1740dde6238419b68d1ca5de3acc3ba00682c78
Denis Narminov~ <narminovd@gmail.com>
2026-02-06 21:42:34 +0300

    Fix: Update unit tests to match Trip struct members

--- TravelTests/tst_traveltests.cpp
+++ TravelTests/tst_traveltests.cpp
@@ -1,73 +1,66 @@
 #include <QtTest>
 #include <QJsonObject>
-
-// --- Имитация структуры Trip для изоляции теста ---
-struct TripMock {
-    QString from;
-    QString to;
-    QDate date;
-    double cost;
-    int people;
-
-    QJsonObject toJson() const {
-        QJsonObject json;
-        json["from"] = from;
-        json["to"] = to;
-        json["cost"] = cost;
-        json["people"] = people;
-        return json;
-    }
-};
+#include <QFile>
+#include "../trip.h"
+#include "../tripmanager.h"
+#include "../priceservice.h"
 
 class TravelTests : public QObject
 {
     Q_OBJECT
 
 private slots:
-    // Тест 1: Проверка создания объекта
-    void testTripCreation() {
-        TripMock trip;
+    void testTripStructure() {
+        Trip trip;
         trip.from = "Moscow";
         trip.to = "Paris";
         trip.cost = 50000;
+        trip.travelers = 1;
+        trip.date = QDate(2026, 1, 1);
 
         QCOMPARE(trip.from, QString("Moscow"));
         QCOMPARE(trip.to, QString("Paris"));
         QCOMPARE(trip.cost, 50000.0);
+        QCOMPARE(trip.travelers, 1);
     }
 
-    // Тест 2: Проверка расчета бюджета
-    void testBudgetCalculation() {
-        double ticketPrice = 15000.0;
-        int people = 3;
-        double total = ticketPrice * people;
-
-        QCOMPARE(total, 45000.0);
-    }
-
-    // Тест 3: Проверка JSON
-    void testJsonSerialization() {
-        TripMock trip;
+    void testJsonConversion() {
+        Trip trip;
         trip.from = "Dubai";
         trip.to = "London";
         trip.cost = 100.0;
-        trip.people = 2;
+        trip.travelers = 2;
+        trip.date = QDate(2026, 5, 20);
 
         QJsonObject json = trip.toJson();
 
         QVERIFY(!json.isEmpty());
         QCOMPARE(json["from"].toString(), QString("Dubai"));
-        QCOMPARE(json["people"].toInt(), 2);
+        QCOMPARE(json["to"].toString(), QString("London"));
+        QCOMPARE(json["travelers"].toInt(), 2);
     }
 
-    // Тест 4: Валидация дат
-    void testDateValidation() {
-        QDate today = QDate::currentDate();
-        QDate tripDate = today.addDays(10);
-        QVERIFY(tripDate > today);
+    void testTripManagerFileOperations() {
+        Trip t1;
+        t1.from = "TestCity";
+        t1.to = "AnotherCity";
+        t1.cost = 123.0;
+        t1.travelers = 1;
+        t1.date = QDate::currentDate();
+
+        QList<Trip> trips;
+        trips.append(t1);
+
+        TripManager::saveTrips(trips);
+
+        QString path = TripManager::getFilePath();
+        QVERIFY(QFile::exists(path));
+
+        QList<Trip> loadedTrips = TripManager::loadTrips();
+        QVERIFY(!loadedTrips.isEmpty());
+        QCOMPARE(loadedTrips.first().from, QString("TestCity"));
     }
 };
 
 QTEST_APPLESS_MAIN(TravelTests)
-
 #include "tst_traveltests.moc"
